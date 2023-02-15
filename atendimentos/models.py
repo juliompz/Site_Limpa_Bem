4ab33b4369e3372299c07644b3334a67bfdb9444
julio cesar <juliokreds@gmail.com>
2023-02-15 08:54:27 -0300

    Atendimento Model corrigido

--- atendimentos/models.py
+++ atendimentos/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User, Group
 
 from servicos.models import Servico
 from clientes.models import Cliente
 
+
+
 class Atendimento(models.Model):
 
-    servico = models.ForeignKey(Servico, on_delete = models.CASCADE)
-    atendente = models.CharField(max_length=100)
-    helper = models.CharField(max_length=100)
+    servico = models.ForeignKey(Servico, on_delete = models.DO_NOTHING)
+
+    atendente = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING,
+        limit_choices_to={'groups__name': 'Atendente'}, related_name='Atendente')
+
+    helper = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        limit_choices_to={'groups__name': 'Helper'},  related_name='Helper')
 
     FORMA_PAGAMENTO = (
 
@@ -22,8 +32,14 @@ class Atendimento(models.Model):
         ('N', 'NÃO')
     )
 
-    gerente_desc = models.CharField(max_length= 1, choices=GERENTE_DESCONTO, blank=False, null=False, default='N')    
-    forma_pgto = models.CharField(max_length= 2, choices=FORMA_PAGAMENTO, blank= False, null= False, default= 'D')
+    gerente_desc = models.CharField(max_length= 1, 
+                                    choices=GERENTE_DESCONTO, blank=False,
+                                    null=False, default='N')    
+
+    forma_pgto = models.CharField(max_length= 2, choices=FORMA_PAGAMENTO, 
+                                    blank= False, null= False, 
+                                    default= 'D')
+
     data_atendimento = models.DateTimeField(auto_now_add = True)
     data_agendada = models.DateTimeField(auto_now_add = False, blank= False, null= False)
 
@@ -35,8 +51,10 @@ class Atendimento(models.Model):
 
     )
     
-    situacao = models.CharField(max_length=1, choices=SITUACAO, blank=False, null=False, default='P')
-    cliente = models.ForeignKey(Cliente, on_delete= models.CASCADE)
+    situacao = models.CharField(max_length=1, choices=SITUACAO, 
+                                blank=False, null=False, 
+                                default='P')
+    cliente = models.ForeignKey(Cliente, on_delete= models.DO_NOTHING)
 
     def __str__(self) -> str:
         return self.cliente.nome
